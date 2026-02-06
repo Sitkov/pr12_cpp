34a5b00e7ee453384a9aa4d2ae680d67f5509be8
Sitkov <154762708+Sitkov@users.noreply.github.com>
2026-02-06 23:50:11 +0500

    Add files via upload

---
+++ Client.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+
+#define MUTEX L"printer"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    HANDLE hMutex = OpenMutex(SYNCHRONIZE, FALSE, MUTEX);
+    if (!hMutex)
+    {
+        cout << "Принтер не найден!\n";
+        return GetLastError();
+    }
+
+    while (true)
+    {
+        cout << "\n1 - Отправить на печать \nЛюбая клавиша - выход" << endl << endl;
+        char c = _getch();
+        if (c != '1') break;
+
+        WaitForSingleObject(hMutex, INFINITE);
+
+        cout << "Отправил печатать" << endl;
+
+        ReleaseMutex(hMutex);
+
+        cout << "Ожидание завершения печати..." << endl;
+
+        WaitForSingleObject(hMutex, INFINITE);
+
+        cout << "Печать завершена" << endl;
+
+        ReleaseMutex(hMutex);
+    }
+
+    CloseHandle(hMutex);
+    return 0;
+}
---
+++ pr12_cpp.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <ctime>
+
+using namespace std;
+
+#define MUTEX L"printer"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+
+    HANDLE hMutex = CreateMutex(NULL, FALSE, MUTEX);
+    if (!hMutex) {
+        cout << "Ошибка создания мьютекса: " << GetLastError() << endl;
+        return GetLastError();
+    }
+
+    cout << "Принтер запущен" << endl;
+
+    DWORD lastClientTime = GetTickCount();
+
+    while (true)
+    {
+        DWORD result = WaitForSingleObject(hMutex, 600000);
+
+        if (result == WAIT_TIMEOUT)
+        {
+            cout << "Принтер умер" << endl;
+            break;
+        }
+
+        if (result == WAIT_OBJECT_0)
+        {
+            lastClientTime = GetTickCount();
+
+            cout << "Печатаю..." << endl;
+
+            int printTime = 5000 + rand() % 10000; 
+            Sleep(printTime);
+
+            if (printTime > 10000) {
+                cout << "Принтер скушал бумагу.." << endl;
+            }
+            else {
+                cout << "Успешно" << endl;;
+            }
+
+            ReleaseMutex(hMutex);
+        }
+    }
+    CloseHandle(hMutex);
+    return 0;
+}
