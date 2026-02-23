7b6e13bb0cecd29afe437ad458d7f84072f383b5
Sitkov <154762708+Sitkov@users.noreply.github.com>
2026-02-23 18:09:28 +0500

    Refactor Client.cpp for better error handling
    Added error handling for printer not found and improved output messages.

--- Client.cpp
+++ Client.cpp
@@ -1,43 +1,38 @@
-﻿#include <iostream>
+#include <iostream>
 #include <windows.h>
 #include <conio.h>
 
 using namespace std;
 
-#define MUTEX L"printer"
-#define EVENT L"event"
-
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    //HANDLE hEvent = OpenEvent(EVENT_MODIFY_STATE, FALSE, EVENT);
+    cout << "-- КЛИЕНТ --" << endl;
+
     HANDLE hMutex = OpenMutex(SYNCHRONIZE, FALSE, L"printer");
     if (!hMutex) {
-        cout << "Принтер не включен" << endl;
+        cout << "Принтер не найден. Ошибка: " << GetLastError() << endl;
+        return GetLastError();
     }
 
-    while (true) {
-        cout << "\n1. Отправить на печать" << endl;
-        cout << "Любая клавиша = Выйти\n" << endl;
+    cout << "Принтер обнаружен" << endl;
 
-        char input = _getch();
-        if (input != '1') {
-            cout << "Пока!" << endl;
-            break;
-        }
-        WaitForSingleObject(hMutex, INFINITE);
-        cout << "Отправил в очередь" << endl;
+    while (true)
+{
+    cout << "\n1. Отправить на печать" << endl;
+    cout << "Любая клавиша = Выйти\n" << endl;
 
-
-        ReleaseMutex(hMutex);
-
-        WaitForSingleObject(hMutex, INFINITE);
-
-        cout << "Печать завершена!!!\n" << endl;
-
-        ReleaseMutex(hMutex);
+    char input = _getch();
+    if (input != '1') {
+        cout << "Пока!" << endl;
+        break;
     }
-    if (!hMutex)
-        CloseHandle(hMutex);
+    WaitForSingleObject(hMutex, INFINITE);
+    cout << "Отправил в очередь" << endl;
+    ReleaseMutex(hMutex);
+}
+
+    CloseHandle(hMutex);
+    return 0;
 }
