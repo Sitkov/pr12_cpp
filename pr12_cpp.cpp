225f6ab0d9e1622ce527a6d8a9e1025fa80389bd
Sitkov <154762708+Sitkov@users.noreply.github.com>
2026-02-23 18:09:14 +0500

    Refactor printer logic and update client execution
    Refactor printer process handling and improve client launch message.

--- pr12_cpp.cpp
+++ pr12_cpp.cpp
@@ -1,73 +1,68 @@
-﻿
 #include <iostream>
 #include <windows.h>
 #include <ctime>
 
 using namespace std;
 
-
-#define EVENT L"event"
-
 int main()
 {
     setlocale(LC_ALL, "Russian");
     srand(time(NULL));
-    
-    //HANDLE hEvent = CreateEvent(NULL, FALSE, FALSE, EVENT);
+
+    cout << "<-- ПРИНТЕР -->" << endl;
+
     HANDLE hMutex = CreateMutex(NULL, FALSE, L"printer");
     if (!hMutex) {
         cout << "Ошибка создания мьютекса: " << GetLastError() << endl;
         return GetLastError();
     }
 
-    wchar_t appName[] = L"C:\\Sitkov\\pr12_cpp\\x64\\Debug\\Client.exe";
+    cout << "Принтер ждет клиентов" << endl;
+
+    wchar_t appName[] = L"C:\\Users\\CustomPC\\Desktop\\pr12_cpp\\x64\\Debug\\Client.exe";
 
     PROCESS_INFORMATION pi{};
     STARTUPINFO si{};
-
     si.cb = sizeof(si);
 
     if (!CreateProcess(NULL, appName, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
-        cout << "Клиент не создан" << endl;
+        cout << "Клиент не создан. Ошибка: " << GetLastError() << endl;
     }
-
-    cout << "Принтер запущен" << endl;
-
-    DWORD lastClientTime = GetTickCount();
+    else {
+        cout << "Клиент запущен" << endl;
+    }
 
     while (true)
     {
-        DWORD result = WaitForSingleObject(hMutex, 600000);
-        //WaitForSingleObject(hEvent, INFINITE);
-
-        if (result == WAIT_TIMEOUT)
-        {
-            cout << "Принтер умер" << endl;
-            break;
-        }
+        DWORD result = WaitForSingleObject(hMutex, 0);
 
         if (result == WAIT_OBJECT_0) {
-            cout << "Печатаю..." << endl;
+            ReleaseMutex(hMutex);
+        }
+        else if (result == WAIT_TIMEOUT) {
+            WaitForSingleObject(hMutex, INFINITE);
+
+            cout << "Начинаю печатать" << endl;
 
             int printTime = 5000 + rand() % 10000;
+
             Sleep(printTime);
 
             if (printTime > 10000) {
-                cout << "Ошибка печати.." << endl;
+                cout << "Ошибка печати!" << endl;
+                cout << endl;
             }
             else {
-                cout << "Успешно" << endl;;
+                cout << "Закончил печать" << endl;
+                cout << endl;
             }
-            ReleaseMutex(hMutex);
-        }
-        else 
-        {
-            Sleep(2000);
-            cout << "Принтер готов к печати" << endl;
-            
+
             ReleaseMutex(hMutex);
         }
     }
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
     CloseHandle(hMutex);
     return 0;
 }
